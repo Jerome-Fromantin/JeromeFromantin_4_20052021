779bd2b6c1a75edfbddc13844c28d1e3eb667bc1
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-06-02 17:00:29 +0200

    Améliorations du code HTML/CSS et des validations du formulaire en JS

--- modal.js
+++ modal.js
@@ -22,75 +22,123 @@ function launchModal() {
 
 // Fonction de validation des entrées du formulaire.
 function validate() {
-  var firstName = document.forms["reserve"]["first"]; // Variable du prénom.
-  var lastName = document.forms["reserve"]["last"]; // Variable du nom.
+
+  /*
+  Insérer éventuellement ici les 3 premières lignes
+  de chaque "if" après le règlement du problème
+  de répétition des différents messages d'erreurs.
+  */
+
+  let firstName = document.forms["reserve"]["first"]; // Variable du prénom.
   if (firstName.value == "") { // Si le prénom n'est pas rempli.
-    if (lastName.value == "") { // Si le nom n'est pas rempli non plus.
-      alert("Veuillez ne pas oublier d'écrire votre prénom et votre nom.");
-      firstName.focus();
-      return false;
-    }
-    // Si le nom n'est pas vide mais qu'il a seulement 1 caractère ou plus de 30.
-    else if (lastName.value.length < 2 || lastName.value.length > 30) {
-      alert("Veuillez écrire un nom contenant entre 2 et 30 caractères inclus.");
-      lastName.focus();
-      return false;
-    }
-    // Si le prénom n'est pas rempli mais que le nom est valide.
-    alert("Veuillez ne pas oublier d'écrire votre prénom.");
+    let firstErrMess = document.createElement("p"); // On crée une variable qui contient un élément "p" créé.
+    let firstErr = document.getElementById("first-err"); // On crée une variable qui récupère l'élément parent.
+    firstErr.appendChild(firstErrMess); // On insère l'élément enfant dans l'élément parent.
+    firstErrMess.textContent = "Veuillez ne pas oublier d'écrire votre prénom."
+    first.style.border = "2px #e54858 solid";
     firstName.focus();
     return false;
   }
+  /*
+  else if (firstName.value.length >= 2 && firstName.value.length <= 20) { // Si le prénom est valide.
+    let firstErrMess = document.createElement("p");
+    let firstErr = document.getElementById("first-err");
+    firstErr.appendChild(firstErrMess);
+    firstErrMess.textContent = "Votre prénom est validé !"
+    first.style.border = "2px #279e7a solid";
+    lastName.focus();
+    return false;
+  }
+  */
   // Si le prénom n'est pas vide mais qu'il a seulement 1 caractère ou plus de 20.
   else if (firstName.value.length < 2 || firstName.value.length > 20) {
-    alert("Veuillez écrire un prénom contenant entre 2 et 20 caractères inclus.");
+    let firstErrMess = document.createElement("p");
+    let firstErr = document.getElementById("first-err");
+    firstErr.appendChild(firstErrMess);
+    firstErrMess.textContent = "Veuillez écrire un prénom contenant entre 2 et 20 caractères inclus.";
+    first.style.border = "2px #e54858 solid";
     firstName.focus();
     return false;
   }
 
-  // Si le prénom est valide mais que le nom n'est pas rempli.
-  if (lastName.value == "") {
-    alert("Veuillez ne pas oublier d'écrire votre nom.");
+  let lastName = document.forms["reserve"]["last"]; // Variable du nom.
+  if (lastName.value == "") { // Si le nom n'est pas rempli.
+    let lastErrMess = document.createElement("p");
+    let lastErr = document.getElementById("last-err");
+    lastErr.appendChild(lastErrMess);
+    lastErrMess.textContent = "Veuillez ne pas oublier d'écrire votre nom.";
+    last.style.border = "2px #e54858 solid";
     lastName.focus();
     return false;
   }
   // Si le nom n'est pas vide mais qu'il a seulement 1 caractère ou plus de 30.
   else if (lastName.value.length < 2 || lastName.value.length > 30) {
-    alert("Veuillez écrire un nom contenant entre 2 et 30 caractères inclus.");
+    let lastErrMess = document.createElement("p");
+    let lastErr = document.getElementById("last-err");
+    lastErr.appendChild(lastErrMess);
+    lastErrMess.textContent = "Veuillez écrire un nom contenant entre 2 et 30 caractères inclus.";
+    last.style.border = "2px #e54858 solid";
     lastName.focus();
     return false;
   }
   
-  var email = document.forms["reserve"]["email"]; // Variable de l'e-mail.
-  if (email.value == "") {
-    alert("Veuillez ne pas oublier d'écrire votre adresse e-mail.");
+  let email = document.forms["reserve"]["email"]; // Variable de l'e-mail.
+  if (email.value == "") { // Si l'e-mail n'est pas rempli.
+    let emailErrMess = document.createElement("p");
+    let emailErr = document.getElementById("email-err");
+    emailErr.appendChild(emailErrMess);
+    emailErrMess.textContent = "Veuillez ne pas oublier d'écrire votre adresse e-mail.";
+    email.style.border = "2px #e54858 solid";
+    email.focus();
+    return false;
+  }
+  // Si l'e-mail a cette valeur de test uniquement.
+  else if (email.value == "test@mail.com") {
+    let emailErrMess = document.createElement("p");
+    let emailErr = document.getElementById("email-err");
+    emailErr.appendChild(emailErrMess);
+    emailErrMess.textContent = "Ceci est une valeur de test.";
+    email.style.border = "2px #e54858 solid";
     email.focus();
     return false;
   }
   
-  var birthdate = document.forms["reserve"]["birthdate"]; // Variable de la date de naissance.
-  if (birthdate.value == "") {
-    alert("Veuillez ne pas oublier d'indiquer votre date de naissance.");
+  let birthdate = document.forms["reserve"]["birthdate"]; // Variable de la date de naissance.
+  if (birthdate.value == "") { // Si la date de naissance n'est pas remplie.
+    let birthdateErrMess = document.createElement("p");
+    let birthdateErr = document.getElementById("birthdate-err");
+    birthdateErr.appendChild(birthdateErrMess);
+    birthdateErrMess.textContent = "Veuillez ne pas oublier d'indiquer votre date de naissance.";
+    birthdate.style.border = "2px #e54858 solid";
     birthdate.focus();
     return false;
   }
   
-  /*
-  var sqCheckbox = document.forms["reserve"]["sq-checkbox1"]; // Variable de la case des conditions d'utilisation.
-  if (sqCheckbox.unchecked) {
-    alert("Cette case doit obligatoirement être cochée.");
-    sqCheckbox.focus();
-    return false;
+  let sqCheckbox = document.forms["reserve"]["sq-checkbox1"]; // Variable de la case des conditions d'utilisation.
+  if (sqCheckbox.checked == true) { /*(!sqCheckbox == false)*/
+    alert('La case de "J\'ai lu et accepté..." doit rester cochée.');
+    //return false;
   }
-  */
+  else {
+    //let sqCheckboxErrMess = document.createElement("p");
+    //let sqCheckboxErr = document.getElementById("sqCheckbox-err");
+    //sqCheckboxErr.appendChild(sqCheckboxErrMess);
+    //sqCheckboxErrMess.textContent = "Cette case doit obligatoirement être cochée.";
+    alert("Cette case n'est pas cochée.");
+    //return false;
+  }
 
   alert("Ce formulaire est validé !");
   return true;
 }
 
-// Fermer le formulaire
-/* function closeModal() {
-  modalbg.style.display = "none";
+// Fonction de fermeture du formulaire.
+/*
+function closeModal() {
+let closeBtn = document.getElementById("close-btn");
+closeBtn.addEventListener('click', onClick() {
+  closeBtn.innerHTML = "Coucou !";
+})
+modalbg.style.display = "none";
 }
-Avec le clic sur span class="close".
 */
