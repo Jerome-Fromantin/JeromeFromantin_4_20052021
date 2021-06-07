5f63df554d0b7383f0f3dd03dde6a77c17a8267f
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-06-07 17:05:55 +0200

    Correction d'erreurs et ajout de validations en JS

--- modal.js
+++ modal.js
@@ -49,12 +49,27 @@ function validate() {
   let birthdateErr = document.getElementById("birthdate-mess");
   let birthdateVal = document.getElementById("birthdate-mess");
   
+  let quantity = document.forms["reserve"]["quantity"]; // Input du nombre de tournois.
+  let quantityErr = document.getElementById("quantity-mess");
+  let quantityVal = document.getElementById("quantity-mess");
+  
+  let location1 = document.forms["reserve"]["location1"]; // Inputs des villes.
+  let location2 = document.forms["reserve"]["location2"];
+  let location3 = document.forms["reserve"]["location3"];
+  let location4 = document.forms["reserve"]["location4"];
+  let location5 = document.forms["reserve"]["location5"];
+  let location6 = document.forms["reserve"]["location6"];
+  let locationsErr = document.getElementById("locations-mess");
+  let locationsVal = document.getElementById("locations-mess");
+  
   // Input de la case des conditions d'utilisation.
   let sqCheckbox = document.forms["reserve"]["sq-checkbox1"];
   let sqCheckboxErr = document.getElementById("sq-checkbox-mess");
   let sqCheckboxVal = document.getElementById("sq-checkbox-mess");
 
   if (firstNameLength == 0) { // Si le prénom n'est pas rempli.
+    // Le message s'écrit en rouge.
+    firstErr.style.color = "#e54858";
     // On insère le message dans l'élément.
     firstErr.innerHTML = "<p>Veuillez écrire votre prénom.</p>"
     // On ajoute une bordure rouge au champ de saisie.
@@ -64,6 +79,7 @@ function validate() {
   }
   // Si le prénom n'est pas vide mais qu'il a seulement 1 caractère ou plus de 20.
   else if (firstNameLength < 2 || firstNameLength > 20) {
+    firstErr.style.color = "#e54858";
     firstErr.innerHTML = "<p>Veuillez écrire un prénom contenant entre 2 et 20 caractères inclus.</p>";
     first.style.border = "2px #e54858 solid";
     firstName.focus();
@@ -78,6 +94,7 @@ function validate() {
   }
 
   if (lastNameLength == "") { // Si le nom n'est pas rempli.
+    lastErr.style.color = "#e54858";
     lastErr.innerHTML = "<p>Veuillez écrire votre nom.</p>";
     last.style.border = "2px #e54858 solid";
     lastName.focus();
@@ -85,6 +102,7 @@ function validate() {
   }
   // Si le nom n'est pas vide mais qu'il a seulement 1 caractère ou plus de 30.
   else if (lastNameLength < 2 || lastNameLength > 30) {
+    lastErr.style.color = "#e54858";
     lastErr.innerHTML = "<p>Veuillez écrire un nom contenant entre 2 et 30 caractères inclus.</p>";
     last.style.border = "2px #e54858 solid";
     lastName.focus();
@@ -97,6 +115,7 @@ function validate() {
   }
   
   if (emailLength == "") { // Si l'e-mail n'est pas rempli.
+    emailErr.style.color = "#e54858";
     emailErr.innerHTML = "<p>Veuillez écrire votre adresse e-mail.</p>";
     email.style.border = "2px #e54858 solid";
     email.focus();
@@ -105,6 +124,7 @@ function validate() {
   // Si l'e-mail n'est pas vide et qu'il est valide selon la regex.
   else if (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)) {
     if (email.value == "test@mail.com") { // Mais si l'e-mail a cette valeur de test uniquement.
+      emailErr.style.color = "#e54858";
       emailErr.innerHTML = "<p>Ceci est une adresse de test.</p>";
       email.style.border = "2px #e54858 solid";
       email.focus();
@@ -132,6 +152,7 @@ function validate() {
     }
   }
   else { // Si l'e-mail n'est pas vide et qu'il est invalide.
+    emailErr.style.color = "#e54858";
     emailErr.innerHTML = "<p>Veuillez écrire une adresse e-mail valide.</p>";
     email.style.border = "2px #e54858 solid";
     email.focus();
@@ -139,7 +160,16 @@ function validate() {
   }
   
   if (birthdate.value == "") { // Si la date de naissance n'est pas remplie.
+    birthdateErr.style.color = "#e54858";
     birthdateErr.innerHTML = "<p>Veuillez indiquer votre date de naissance.</p>";
+    birthdate.style.border = "2px #e54858 solid";
+    birthdate.focus();
+    return false;
+  }
+  // Si la date de naissance n'est pas vide mais invalide (> à 100 ans ou < à 18 ans).
+  else if (birthdate.value < "1921-01-01" || birthdate.value > "2003-12-31") {
+    birthdateErr.style.color = "#e54858";
+    birthdateErr.innerHTML = "<p>Veuillez indiquer une année postérieure à 1920 et antérieure à 2004.</p>";
     birthdate.style.border = "2px #e54858 solid";
     birthdate.focus();
     return false;
@@ -150,7 +180,135 @@ function validate() {
     birthdate.style.border = "2px #279e7a solid";
   }
   
+  if (quantity.value == "") { // Si le nombre de tournois n'est pas rempli.
+    quantityErr.style.color = "#e54858";
+    quantityErr.innerHTML = "<p>Veuillez indiquer le nombre de tournois auxquels vous avez participé.</p>";
+    quantity.style.border = "2px #e54858 solid";
+    quantity.focus();
+    return false;
+  }
+  // Si le nombre de tournois n'est pas vide mais inférieur à 1 ou supérieur ou égal à 100.
+  else if (quantity.value < 1 || quantity.value >= 100) {
+    quantityErr.style.color = "#e54858";
+    quantityErr.innerHTML = "<p>Veuillez indiquer un nombre entre 1 et 99 inclus.</p>";
+    quantity.style.border = "2px #e54858 solid";
+    quantity.focus();
+    return false;
+  }
+  else { // Si le nombre de tournois est valide.
+    quantityVal.style.color = "#279e7a";
+    quantityVal.innerHTML = "<p>Ce nombre est validé !</p>";
+    quantity.style.border = "2px #279e7a solid";
+  }
+
+  // Si aucune ville n'est cochée.
+  if (location1.checked == false && location2.checked == false && location3.checked == false
+  && location4.checked == false && location5.checked == false && location6.checked == false) {
+    locationsErr.style.color = "#e54858";
+    locationsErr.innerHTML = "<p>Vous devez cocher au moins l'une des villes.</p>";
+    return false;
+  }
+  else {
+    // Si une seule ville est cochée.
+    if (location1.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché " + location1.value + ", votre choix est validé !</p>";
+    }
+    if (location2.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché " + location2.value + ", votre choix est validé !</p>";
+    }
+    if (location3.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché " + location3.value + ", votre choix est validé !</p>";
+    }
+    if (location4.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché " + location4.value + ", votre choix est validé !</p>";
+    }
+    if (location5.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché " + location5.value + ", votre choix est validé !</p>";
+    }
+    if (location6.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché " + location6.value + ", votre choix est validé !</p>";
+    }
+    // Si deux villes sont cochées.
+    if (location1.checked && location2.checked || location1.checked && location3.checked
+      || location1.checked && location4.checked || location1.checked && location5.checked
+      || location1.checked && location6.checked || location2.checked && location3.checked
+      || location2.checked && location4.checked || location2.checked && location5.checked
+      || location2.checked && location6.checked || location3.checked && location4.checked
+      || location3.checked && location5.checked || location3.checked && location6.checked
+      || location4.checked && location5.checked || location4.checked && location6.checked
+      || location5.checked && location6.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché deux villes, votre choix est validé !</p>";
+    }
+    // Si trois villes sont cochées.
+    if (location1.checked && location2.checked && location3.checked
+      || location1.checked && location2.checked && location4.checked
+      || location1.checked && location2.checked && location5.checked
+      || location1.checked && location2.checked && location6.checked
+      || location1.checked && location3.checked && location4.checked
+      || location1.checked && location3.checked && location5.checked
+      || location1.checked && location3.checked && location6.checked
+      || location1.checked && location4.checked && location5.checked
+      || location1.checked && location4.checked && location6.checked
+      || location1.checked && location5.checked && location6.checked
+      || location2.checked && location3.checked && location4.checked
+      || location2.checked && location3.checked && location5.checked
+      || location2.checked && location3.checked && location6.checked
+      || location2.checked && location4.checked && location5.checked
+      || location2.checked && location4.checked && location6.checked
+      || location2.checked && location5.checked && location6.checked
+      || location3.checked && location4.checked && location5.checked
+      || location3.checked && location4.checked && location6.checked
+      || location3.checked && location5.checked && location6.checked
+      || location4.checked && location5.checked && location6.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché trois villes, votre choix est validé !</p>";
+    }
+    // Si quatre villes sont cochées.
+    if (location1.checked && location2.checked && location3.checked && location4.checked
+      || location1.checked && location2.checked && location3.checked && location5.checked
+      || location1.checked && location2.checked && location3.checked && location6.checked
+      || location1.checked && location2.checked && location4.checked && location5.checked
+      || location1.checked && location2.checked && location4.checked && location6.checked
+      || location1.checked && location2.checked && location5.checked && location6.checked
+      || location1.checked && location3.checked && location4.checked && location5.checked
+      || location1.checked && location3.checked && location4.checked && location6.checked
+      || location1.checked && location3.checked && location5.checked && location6.checked
+      || location1.checked && location4.checked && location5.checked && location6.checked
+      || location2.checked && location3.checked && location4.checked && location5.checked
+      || location2.checked && location3.checked && location4.checked && location6.checked
+      || location2.checked && location3.checked && location5.checked && location6.checked
+      || location2.checked && location4.checked && location5.checked && location6.checked
+      || location3.checked && location4.checked && location5.checked && location6.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché quatre villes, votre choix est validé !</p>";
+    }
+    // Si cinq villes sont cochées.
+    if (location1.checked && location2.checked && location3.checked && location4.checked && location5.checked
+      || location1.checked && location2.checked && location3.checked && location4.checked && location6.checked
+      || location1.checked && location2.checked && location3.checked && location5.checked && location6.checked
+      || location1.checked && location2.checked && location4.checked && location5.checked && location6.checked
+      || location1.checked && location3.checked && location4.checked && location5.checked && location6.checked
+      || location2.checked && location3.checked && location4.checked && location5.checked && location6.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché cinq villes, votre choix est validé !</p>";
+    }
+    // Si toutes les villes sont cochées.
+    if (location1.checked && location2.checked && location3.checked
+      && location4.checked && location5.checked && location6.checked) {
+      locationsVal.style.color = "#279e7a";
+      locationsVal.innerHTML = "<p>Vous avez coché toutes les villes, votre choix est validé !</p>";
+    }
+  }
+  
   if (sqCheckbox.checked == false) { // Si la case des conditions est décochée.
+    sqCheckboxErr.style.color = "#e54858";
     sqCheckboxErr.innerHTML = "<p>Vous devez avoir lu et accepté les conditions d'utilisation.</p>";
     return false;
   }
@@ -159,15 +317,33 @@ function validate() {
     sqCheckboxVal.innerHTML = "<p>Merci d'avoir lu et accepté les conditions d'utilisation.</p>";
   }
   
-  alert("Ce formulaire est validé !");
+  //alert("Ce formulaire est validé !");
   return true;
+  function openValid() { // Fonction d'ouverture de la validation.
+    let validContent = document.querySelector(".valid-content");
+    validContent.style.display = "block";
+  }
 }
 
-// Fonction d'écoute du clic sur bouton de fermeture pour la fonction suivante.
-let closeBtn = document.getElementById("close-btn");
-closeBtn.addEventListener('click', closeModal);
+// Fonction d'écoute du clic sur bouton de fermeture du formulaire pour la fonction suivante.
+let closeBtn1 = document.getElementById("close-btn1");
+closeBtn1.addEventListener('click', closeModal);
 
 // Fonction de fermeture du formulaire.
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+// Fonction d'écoute du clic sur bouton de fermeture de la validation pour la fonction suivante.
+let validContent = document.querySelector(".valid-content");
+let closeBtn2 = document.getElementById("close-btn2");
+closeBtn2.addEventListener('click', closeValid);
+
+// Fonction de fermeture de la validation.
+function closeValid(event) {
+  event.stopPropagation();
+  //let validation = document.getElementById("validation");
+  //validation.style.color = "red";//"block";
+  validContent.style.display = "none";
+  alert("Ferme-toi !");
 }
