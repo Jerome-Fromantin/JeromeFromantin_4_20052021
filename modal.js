b272b4c44b715a03bba75ed56177349c10cdd382
Jerome-Fromantin <68106114+Jerome-Fromantin@users.noreply.github.com>
2021-06-21 09:50:14 +0200

    Suppr modif en ligne modal.js

--- modal.js
+++ modal.js
@@ -549,8 +549,8 @@ validCloseModalBtn.addEventListener('click', validCloseModal);
 
 // Fonction de validation finale et fermeture du modal.
 function validCloseModal() {
-  modalbg.style.display = "none";
-  validContent.style.display = "none";
   document.getElementById("reserve").submit();
+  //modalbg.style.display = "none";
+  //validContent.style.display = "none";
   //document.getElementById("reserve").reset();
 }
